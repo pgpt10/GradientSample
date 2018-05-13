62838606adf3e1b6f68fa89587f74fc167cbaebd
Payal Gupta <payal.gupta@99acres.com>
2018-05-13 18:00:45 +0530

    first commit..

---
+++ Gradient/DesignableView.swift
@@ -0,0 +1,46 @@
+//
+//  DesignableView.swift
+//  Gradient
+//
+//  Created by Payal Gupta on 12/05/18.
+//  Copyright © 2018 Payal Gupta. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView
+{
+     @IBInspectable var gradientColor1: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientColor2: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientStartPoint: CGPoint = .zero {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientEndPoint: CGPoint = CGPoint(x: 0, y: 1) {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    private func setGradient()
+    {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+        gradientLayer.startPoint = self.gradientStartPoint
+        gradientLayer.endPoint = self.gradientEndPoint
+        gradientLayer.frame = self.bounds
+        self.layer.addSublayer(gradientLayer)
+    }
+}
---
+++ Gradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gradient
+//
+//  Created by Payal Gupta on 12/05/18.
+//  Copyright © 2018 Payal Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
+
